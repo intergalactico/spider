213fe6c057db949724b1e3aef7916a5582085e15
Junaid Rasheed <junaidrsd@hotmail.com>
2018-07-10 12:24:59 +0300

    included all files in package

--- examples/mindNode.js
+++ examples/mindNode.js
@@ -1,6 +1,6 @@
 import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MindNode from './mindNode/index.jsx';
+import MindNode from './mindNode/index.js';
 
 ReactDOM.render(<MindNode />, document.getElementById('__react-content'));
