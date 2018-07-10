1187ec434b021cf6812fe60e0f90f8b03fee4264
Junaid Rasheed <junaidrsd@hotmail.com>
2018-07-10 10:24:14 +0300

    jsx extention changed to js in order to fix build

---
+++ src/shapes/index.js
@@ -0,0 +1,13 @@
+import Link from './Link.js';
+import Node from './Node.js';
+import Circle from './Circle.js';
+import Text from './Text.js';
+import Rect from './Rect.js';
+
+export default {
+  Link,
+  Node,
+  Circle,
+  Rect,
+  Text,
+};
